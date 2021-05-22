924018d080c7d1dce58c609cb8017673585aa70e
Umutambyi Gad <umutambyig@gmail.com>
2021-05-22 12:27:00 +0100

    Create path for blogs

--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+
+app_name = 'website'
+
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('blogs', views.blogs, name='blogs')
 ]
