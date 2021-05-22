535aa7caad5e08ab66964509c888b7b147c3080e
Umutambyi Gad <umutambyig@gmail.com>
2021-05-22 12:25:00 +0100

    Create view for blog

--- website/views.py
+++ website/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+def blogs(request):
+    return render(request, 'blogs.html')
