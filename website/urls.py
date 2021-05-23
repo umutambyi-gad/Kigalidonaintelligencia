4ada4da15c36be21a68c1b991348bf1821f6cd9c
Umutambyi Gad <umutambyig@gmail.com>
2021-05-23 15:46:00 +0100

    Add path for contact page

--- website/urls.py
+++ website/urls.py
@@ -6,5 +6,6 @@ app_name = 'website'
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('blogs', views.blogs, name='blogs')
+    path('blogs', views.blogs, name='blogs'),
+    path('contact', views.contact, name='contact')
 ]
