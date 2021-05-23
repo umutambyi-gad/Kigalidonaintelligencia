7db3b9bbf79b65eb298ce99338b0d4d069055316
Umutambyi Gad <umutambyig@gmail.com>
2021-05-23 14:20:00 +0100

    Add path for book detail and blog detail

--- website/urls.py
+++ website/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about-us', views.aboutUs, name='aboutUs'),
     path('books', views.books, name='books'),
+    path('books/book-title-slug', views.bookSingle, name='book-single'),
     path('blogs', views.blogs, name='blogs'),
+    path('blogs/blog-title-slug', views.blogSingle, name='blog-single'),
     path('contact', views.contact, name='contact')
 ]
