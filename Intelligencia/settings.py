8abea283b07df6294ffba5df7b2e2eafb3990086
Umutambyi Gad <umutambyig@gmail.com>
2021-05-24 16:07:00 +0100

    Configure email settings

--- Intelligencia/settings.py
+++ Intelligencia/settings.py
@@ -123,6 +123,13 @@ USE_L10N = True
 
 USE_TZ = True
 
+EMAIL_HOST = config('EMAIL_HOST', default='localhost')
+EMAIL_PORT = config('EMAIL_PORT', cast=int, default=1025)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+EMAIL_USE_SSL = config('EMAIL_USE_TLS', default=False, cast=bool)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=False, cast=bool)
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default='')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
