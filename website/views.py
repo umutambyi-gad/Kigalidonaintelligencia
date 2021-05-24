941d770b78c6196b008bc83346b9acb39b91476b
Umutambyi Gad <umutambyig@gmail.com>
2021-05-24 16:30:00 +0100

    Processed input and send email

--- website/views.py
+++ website/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail
 
 
 # Create your views here.
@@ -21,4 +22,19 @@ def blogSingle(request, blog_id, blog_title_slug):
     return render(request, 'blog-single.html')
 
 def contact(request):
+    if request.method == 'POST':
+        sender_firstname = request.POST['firstname']
+        sender_lastname = request.POST['lastname']
+        sender_names = f'{sender_firstname} {sender_lastname}'
+        subject = request.POST['subject']
+        sender_email = request.POST['email']
+        message = request.POST['message']
+
+        send_mail(subject, # subject
+            f'{sender_names} sends {message}', # message
+			sender_email, # from_email
+			['umutambyig@gmail.com']
+        )
+        return redirect('/contact')
+
     return render(request, 'contact.html')
