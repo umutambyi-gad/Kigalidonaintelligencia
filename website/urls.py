463a4a34b8de78b49cde5b1849a673a4204df496
Umutambyi Gad <umutambyig@gmail.com>
2021-05-24 15:17:00 +0100

    Add path for books page

--- website/urls.py
+++ website/urls.py
@@ -6,6 +6,7 @@ app_name = 'website'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('books', views.books, name='books'),
     path('blogs', views.blogs, name='blogs'),
     path('contact', views.contact, name='contact')
 ]
