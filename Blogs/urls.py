bc2c8c3b453abd1a19bef981b10a840c61b6f895
Umutambyi Gad <umutambyig@gmail.com>
2021-05-25 14:28:00 +0100

    Add blogs template directory

--- Blogs/urls.py
+++ Blogs/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'Blogs'
 
 urlpatterns = [
-    path('blogs', views.blogs, name='blogs'),
-    path('blogs/<int:blog_id>/<slug:blog_title_slug>', views.blogSingle, name='blog-single'),
+    path('', views.blogs, name='blogs'),
+    path('<int:blog_id>/<slug:blog_title_slug>', views.blogSingle, name='blog-single'),
 ]
