443fc1c4db89c3e81a3c12ec410e05c37f625810
Umutambyi Gad <umutambyig@gmail.com>
2021-05-26 12:44:00 +0100

    COnfigure media root in setting

--- Intelligencia/settings.py
+++ Intelligencia/settings.py
@@ -148,6 +148,9 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 STATIC_ROOT = os.path.join(BASE_DIR, "assets")
 
 # Default primary key field type
