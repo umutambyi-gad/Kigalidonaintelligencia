6595c7655d9d48d3b924e3faea92d0039dd84d95
Umutambyi Gad <umutambyig@gmail.com>
2021-05-26 14:50:00 +0100

    Add about us models

--- AboutUs/models.py
+++ AboutUs/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class AboutUs(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='media/home/images')
+    description = models.TextField()
+    added_date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.background_title
+    class Meta:
+        verbose_name_plural = 'About us section'
