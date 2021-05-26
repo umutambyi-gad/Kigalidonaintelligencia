94ae3890e99b0f1e30bfed59fa65440876526863
Umutambyi Gad <umutambyig@gmail.com>
2021-05-26 12:44:00 +0100

    COnfigure media path in urls

--- Intelligencia/urls.py
+++ Intelligencia/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', include('Home.urls')),
@@ -25,3 +26,5 @@ urlpatterns = [
     path('contact/', include('Contact.urls')),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
