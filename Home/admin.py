3a98fbcf19220eafeb2270eeeff8f2403a6938fa
Umutambyi Gad <umutambyig@gmail.com>
2021-05-26 13:37:00 +0100

    Register home models admin

--- Home/admin.py
+++ Home/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import (
+    HomeBackground,
+    HomeWelcoming
+)
+
 
 # Register your models here.
+class HomeBackgroundAdmin(admin.ModelAdmin):
+	fieldsets = (
+	(None, {
+		'fields': ('title', 'description')
+		}),
+	('Image section', {
+		'fields': ('image', )
+		})
+	)
+	list_display = ('title', 'added_date')
+
+admin.site.register(HomeBackground, HomeBackgroundAdmin)
+
+
+class HomeWelcomingAdmin(admin.ModelAdmin):
+	fieldsets = (
+	(None, {
+		'fields': ('title', 'description')
+		}),
+	('Image section', {
+		'fields': ('image', )
+		})
+	)
+	list_display = ('title', 'added_date')
+
+admin.site.register(HomeWelcoming, HomeWelcomingAdmin)
