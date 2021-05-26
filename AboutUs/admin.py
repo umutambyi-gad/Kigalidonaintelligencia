0fb2e145e87692466f6529bf4920bda4056f6879
Umutambyi Gad <umutambyig@gmail.com>
2021-05-26 15:00:00 +0100

    Register about us models to admin

--- AboutUs/admin.py
+++ AboutUs/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import AboutUs
+
 
 # Register your models here.
+class AboutUsAdmin(admin.ModelAdmin):
+	fieldsets = (
+	(None, {
+		'fields': ('title', 'description')
+		}),
+	('Image section', {
+		'fields': ('image', )
+		})
+	)
+	list_display = ('title', 'added_date')
+
+admin.site.register(AboutUs, AboutUsAdmin)
