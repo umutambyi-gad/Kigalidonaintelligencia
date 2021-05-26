6f62e82981bdb6fa4c074fff894476447ccaf171
Umutambyi Gad <umutambyig@gmail.com>
2021-05-26 13:30:00 +0100

    Add home models

--- Home/models.py
+++ Home/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
+
 # Create your models here.
+class HomeBackground(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='media/home/images')
+    description = models.TextField()
+    added_date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.background_title
+    class Meta:
+        verbose_name_plural = 'Home background section'
+
+
+class HomeWelcoming(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='media/home/images')
+    description = models.TextField()
+    added_date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
+    class Meta:
+        verbose_name_plural = 'Home welcoming section'
