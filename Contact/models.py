7a4a31adafc69237abeac159692ee4fefe2e6fd9
Umutambyi Gad <umutambyig@gmail.com>
2021-05-26 15:15:00 +0100

    Add contact model

--- Contact/models.py
+++ Contact/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Contact(models.Model):
+    phone = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    added_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return 'contacts'
