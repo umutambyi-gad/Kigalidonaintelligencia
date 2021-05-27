a1c08a469b2f6c7d67886a445197ff51af541362
Umutambyi Gad <umutambyig@gmail.com>
2021-05-27 10:11:00 +0100

    Register contact model in admin

--- Contact/admin.py
+++ Contact/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Contact
+
 
 # Register your models here.
+class ContactAdmin(admin.ModelAdmin):
+	fieldsets = (
+	(None, {
+		'fields': ('phone', 'email', 'address')
+		}),
+	)
+	list_display = ('address', 'added_date')
+
+admin.site.register(Contact, ContactAdmin)
