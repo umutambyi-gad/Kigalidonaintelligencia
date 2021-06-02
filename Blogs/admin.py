7253e4d20fe6c8d795b7eaa97e6f7b6e0f014130
Umutambyi Gad <umutambyig@gmail.com>
2021-06-02 15:26:00 +0100

    Register blog models

--- Blogs/admin.py
+++ Blogs/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from .models import (
+    Tags,
+    Blogs
+)
+
 
 # Register your models here.
+class TagsAdmin(admin.ModelAdmin):
+	fieldsets = (
+		(None, {
+			'fields': ('tag', 'added_date')
+		}),
+		
+	)
+	list_display = ('tag', 'added_date')
+
+admin.site.register(Tags, TagsAdmin)
+
+
+class BlogsAdmin(admin.ModelAdmin):
+	fieldsets = (
+		(None, {
+			'fields': ('author', 'title', 'description', 'content', 'author_comment', 'related_tag')
+		}),
+        ('Image section (Required)', {
+            'fields': ('thumbnail', )
+        }),
+        ('Image section (Optional)', {
+			'fields': ('first_image', 'second_image', 'third_image', 'fourth_image', 'fifth_image')
+		}),
+	)
+	list_display = ('author', 'added_date')
+
+admin.site.register(Blogs, BlogsAdmin)
