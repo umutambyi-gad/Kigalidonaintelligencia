202ac5174e0fb7541b7c30c66ba6ee4d3206a7dc
Umutambyi Gad <umutambyig@gmail.com>
2021-06-03 16:33:00 +0100

    register categories in admin

--- Blogs/admin.py
+++ Blogs/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import (
+    Categories,
     Tags,
     Blogs
 )
@@ -18,10 +19,22 @@ class TagsAdmin(admin.ModelAdmin):
 admin.site.register(Tags, TagsAdmin)
 
 
+class CategoriesAdmin(admin.ModelAdmin):
+	fieldsets = (
+		(None, {
+			'fields': ('category', 'added_date')
+		}),
+		
+	)
+	list_display = ('category', 'added_date')
+
+admin.site.register(Categories, CategoriesAdmin)
+
+
 class BlogsAdmin(admin.ModelAdmin):
 	fieldsets = (
 		(None, {
-			'fields': ('author', 'title', 'description', 'content', 'author_comment', 'related_tag')
+			'fields': ('author', 'title', 'description', 'content', 'author_comment', 'related_tags', 'related_categories')
 		}),
         ('Image section (Required)', {
             'fields': ('thumbnail', )
