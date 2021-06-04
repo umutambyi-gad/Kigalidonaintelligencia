62ede159a9088f9e0052c4ae90c7e4586f1ebdf0
Umutambyi Gad <umutambyig@gmail.com>
2021-06-04 12:28:00 +0100

    Fetch data of who we are section

--- AboutUs/views.py
+++ AboutUs/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from Home.models import TestimonyAdding
+from .models import AboutUs
 
 
 # Create your views here.
 def aboutUs(request):
     testinony_adding = TestimonyAdding.objects.all()
+    About_us = AboutUs.objects.first()
     context = {
-        'testimonies': testinony_adding
+        'testimonies': testinony_adding,
+        'about_us': About_us
     }
     return render(request, 'about-us.html', context=context)
