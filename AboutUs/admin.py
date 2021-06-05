b092be5272f832a85ef072ced3032b49a73c8220
Umutambyi Gad <umutambyig@gmail.com>
2021-06-05 09:44:00 +0100

    Register social media profile in admin

--- AboutUs/admin.py
+++ AboutUs/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import AboutUs
+from .models import (
+	AboutUs,
+	SocialMedia
+)
 
 
 # Register your models here.
@@ -15,3 +18,14 @@ class AboutUsAdmin(admin.ModelAdmin):
 	list_display = ('title', 'added_date')
 
 admin.site.register(AboutUs, AboutUsAdmin)
+
+
+class SocialMediaAdmin(admin.ModelAdmin):
+	fieldsets = (
+		('Social media profiles', {
+			'fields': ('linkedin_profile', 'facebook_profile', 'twitter_profile', 'instagram_profile', 'youtube_profile')
+		}),
+	)
+	list_display = ('user', 'added_date')
+
+admin.site.register(SocialMedia, SocialMediaAdmin)
