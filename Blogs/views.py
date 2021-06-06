a0935cf9628c3305ba68cb76c230e4f6fc64f810
Umutambyi Gad <umutambyig@gmail.com>
2021-06-06 12:11:00 +0100

    Add view for handling new blog composing

--- Blogs/views.py
+++ Blogs/views.py
@@ -12,3 +12,8 @@ def blogSingle(request, blog_id, blog_title_slug):
     context = {}
     context = {**context, **footerContacts(request)}
     return render(request, 'blog-single.html', context=context)
+
+def newBlog(request):
+    context = {}
+    context = {**context, **footerContacts(request)}
+    return render(request, 'new-blog.html', context=context)
