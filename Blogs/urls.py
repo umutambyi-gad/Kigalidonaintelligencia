2a1e44c63bcf9d4c58b2653b9eefbb590ec90864
Umutambyi Gad <umutambyig@gmail.com>
2021-06-06 12:07:00 +0100

    Add url for new blogs composing

--- Blogs/urls.py
+++ Blogs/urls.py
@@ -7,4 +7,5 @@ app_name = 'Blogs'
 urlpatterns = [
     path('', views.blogs, name='blogs'),
     path('<int:blog_id>/<slug:blog_title_slug>', views.blogSingle, name='blog-single'),
+    path('new-blog', views.newBlog, name='new-blog'),
 ]
