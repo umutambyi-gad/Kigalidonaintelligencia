fb312147c1cdf8099d7b7c0f64e95410261415d8
Umutambyi Gad <umutambyig@gmail.com>
2021-06-12 15:27:00 +0100

    Make views counter functioning

--- Blogs/views.py
+++ Blogs/views.py
@@ -13,9 +13,15 @@ def blogs(request):
     return render(request, 'blogs.html', context=context)
 
 def blogSingle(request, blog_id, blog_title_slug):
-    blog = Blogs.objects.first()
+    current_blog = Blogs.objects.get(pk=blog_id)
+
+    if request.method == 'GET':
+        views = current_blog.views + 1
+        current_blog.views = views
+        current_blog.save()
+
     context = {
-        'blog': blog,
+        'blog': current_blog,
     }
     context = {**context, **footerContacts(request)}
     return render(request, 'blog-single.html', context=context)
