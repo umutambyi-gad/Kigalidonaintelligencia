516b00c3f2de8fe9245b514102d25e355d462694
Umutambyi Gad <umutambyig@gmail.com>
2021-06-12 12:23:00 +0100

    Implement blogs dynamically

--- Blogs/views.py
+++ Blogs/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render
 from Contact.views import footerContacts
+from .models import Blogs
 
 
 # Create your views here.
 def blogs(request):
-    context = {}
+    blogs = Blogs.objects.all()
+    context = {
+        'blogs': blogs
+    }
     context = {**context, **footerContacts(request)}
     return render(request, 'blogs.html', context=context)
 
 def blogSingle(request, blog_id, blog_title_slug):
-    context = {}
+    blog = Blogs.objects.first()
+    context = {
+        'blog': blog,
+    }
     context = {**context, **footerContacts(request)}
     return render(request, 'blog-single.html', context=context)
 
