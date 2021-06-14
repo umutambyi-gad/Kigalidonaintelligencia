681e454397ab3972d2a6387450e7971e1197e262
Umutambyi Gad <umutambyig@gmail.com>
2021-06-14 10:21:00 +0100

    Add books models and thier categories

--- Books/models.py
+++ Books/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Categories(models.Model):
+	category = models.CharField(max_length=150)
+	added_date = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.category
+
+	class Meta:
+		verbose_name_plural = 'Categories'
+
+
+class Books(models.Model):
+    athour = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    edition = models.CharField(max_length=255)
+    publisher = models.CharField(max_length=255)
+    thumbnail = models.ImageField(upload_to='media/books/images', null=True, blank=True)
+    short_summary = models.TextField()
+    long_summary = models.TextField()
+    related_tags = models.ManyToManyField(Categories)
+    views = models.IntegerField(default=0)
+    added_date = models.DateTimeField(auto_now_add=True)
