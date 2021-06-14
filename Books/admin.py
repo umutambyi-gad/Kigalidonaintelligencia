d75a7d5cae75df879f83c8a3dd6715fa34c5ff6d
Umutambyi Gad <umutambyig@gmail.com>
2021-06-14 10:34:00 +0100

    Register books and tags model in the admin

--- Books/admin.py
+++ Books/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
+from .models import (
+    Categories,
+    Books
+)
+
 
 # Register your models here.
+class CategoriesAdmin(admin.ModelAdmin):
+	fieldsets = (
+		(None, {
+			'fields': ('category',)
+		}),
+		
+	)
+	list_display = ('category', 'added_date')
+
+admin.site.register(Categories, CategoriesAdmin)
+
+class BooksAdmin(admin.ModelAdmin):
+    fieldsets = (
+		('Book informations', {
+			'fields': ('name', 'edition', 'publisher')
+		}),
+        ('Book thumbnail (Optional)', {
+			'fields': ('thumbnail',)
+		}),
+        ('Book summaries', {
+			'fields': ('short_summary', 'long_summary')
+		}),
+        ('Related', {
+            'fields': ('related_tags',)
+        })
+    )
+
+    def save_model(self, request, obj, form, change):
+        obj.author = request.user
+        super().save_model(request, obj, form, change)
+
+    list_display = ('name', 'added_date')
+
+admin.site.register(Books, BooksAdmin)
