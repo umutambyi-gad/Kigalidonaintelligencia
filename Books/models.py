ce79453b2ba654a5b7e76a7eb56f6c8531aa56c3
Umutambyi Gad <umutambyig@gmail.com>
2021-06-18 10:17:00 +0100

    Add queries as class method on the books models

--- Books/models.py
+++ Books/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.query_utils import Q
 from django.template.defaultfilters import slugify
 from django.shortcuts import reverse
 
@@ -38,6 +39,29 @@ class Books(models.Model):
     def recent_books(cls):
         recents = cls.objects.order_by('-added_date')
         return recents[:9]
+
+    @classmethod
+    def search_book(cls, query):
+        result = cls.objects.filter(
+            Q(name__icontains=query) |
+            Q(edition__icontains=query) |
+            Q(short_summary__icontains=query) |
+            Q(long_summary__icontains=query)
+        )
+
+        return result
+    
+    @classmethod
+    def search_book_with_category(cls, category, query):
+        result = cls.objects.filter(
+            Q(name__icontains=query) |
+            Q(edition__icontains=query) |
+            Q(short_summary__icontains=query) |
+            Q(long_summary__icontains=query) |
+            Q(related_category=Categories.objects.get(category=category))
+        )
+
+        return result
     
     def __str__(self):
         return self.name
