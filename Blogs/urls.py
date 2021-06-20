9dc4596c2e8a8ef97e6bac17fa38800e058d51a5
Umutambyi Gad <umutambyig@gmail.com>
2021-06-20 07:40:00 +0100

    Add url path for searching blog

--- Blogs/urls.py
+++ Blogs/urls.py
@@ -7,5 +7,6 @@ app_name = 'Blogs'
 urlpatterns = [
     path('', views.blogs, name='blogs'),
     path('<int:blog_id>/<slug:blog_title_slug>', views.blogSingle, name='blog-single'),
+    path('search-blogs', views.searchBlog, name='search-blog'),
     path('new-blog', views.newBlog, name='new-blog'),
 ]
