0ba9e6a588108486b4bd9cff7616117cdc927e4d
Umutambyi Gad <umutambyig@gmail.com>
2021-06-29 13:35:00 +0200

    Add root comments models

--- Blogs/models.py
+++ Blogs/models.py
@@ -85,4 +85,17 @@ class Blogs(models.Model):
 
     class Meta:
         verbose_name_plural = 'Blogs'
+
+
+class RootComments(models.Model):
+    commentor = models.CharField(max_length=255)
+    comment = models.TextField()
+    blog = models.ForeignKey(Blogs, on_delete=models.CASCADE)
+    added_date = models.DateTimeField(auto_now_add=True)
     
+    def __str__(self):
+        return self.blog.title
+	
+    class Meta:
+        verbose_name_plural = 'Root Comments'
+
